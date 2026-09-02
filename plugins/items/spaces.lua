8d427f8ff2b995d389ce40bcee8e58fdf052bccb
Daniel Feldman <daniel.salit@hotmail.com>
2026-09-02 17:11:42 +0200

    boilerplate for dynamic popup creation

--- plugins/items/spaces.lua
+++ plugins/items/spaces.lua
@@ -2,7 +2,25 @@ local colors = require("plugins.colors")
 
 sbar.add("event", "aerospace_workspace_change")
 
--- TODO: add space popup logic with predefind popup items for daemon integration
+local function highlight(env, item)
+	if env.SENDER == "mouse.entered" then
+		item:set({
+			background = {
+				color = colors.highlight,
+				height = 24,
+				corner_radius = 9,
+			},
+			label = {
+				color = colors.highlight_text,
+			},
+		})
+	elseif env.SENDER == "mouse.exited" then
+		item:set({
+			background = { color = colors.bar_color },
+			label = { color = colors.highlight },
+		})
+	end
+end
 
 local spaces = { "1", "2", "3", "4", "A", "S" }
 
@@ -50,4 +68,38 @@ for _, space_id in ipairs(spaces) do
 			})
 		end
 	end)
+
+	local back_slot = sbar.add("item", "popup.slot.back", {
+		position = "popup." .. space.name,
+		label = {
+			string = "Back",
+			color = colors.highlight,
+		},
+		drawing = false,
+	})
+
+	back_slot:subscribe({ "mouse.entered", "mouse.exited" }, function(env)
+		highlight(env, back_slot)
+	end)
+
+	back_slot:subscribe("mouse.clicked", function()
+		-- TODO: add call to daemon to --navigate-back
+	end)
+
+	-- pool of standard popups to be populated by daemon
+	for i = 1, 10 do
+		local slot = sbar.add("item", "popup.slot." .. i, {
+			position = "popup." .. space.name,
+			label = { color = colors.highlight },
+			drawing = false,
+		})
+
+		slot:subscribe({ "mouse.entered", "mouse.exited" }, function(env)
+			highlight(env, slot)
+		end)
+
+		slot:subscribe("mouse.clicked", function()
+			-- TODO: add call to daemon to --call-slot or select slot, let daemon check for children or so
+		end)
+	end
 end
